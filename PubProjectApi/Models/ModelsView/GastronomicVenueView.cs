9c29e18b9b71d6155ca2b388202ef2feeed7d386
Szymon <szymon995@gmail.com>
2018-11-21 21:26:50 +0100

    single venue

---
+++ PubProjectApi/Models/ModelsView/GastronomicVenueView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PubProjectApi.Models.ModelsView
+{
+    public class GastronomicVenueView
+    {
+        public IEnumerable<Advertisement> Advertisements { get; set; }
+        public GastronomicVenue GastronomicVenue { get; set; }
+        public double Grade { get; set; }
+    }
+}
