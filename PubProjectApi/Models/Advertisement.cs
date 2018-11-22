71a538ee8110e9ecf575fa063fb7e7fc93ce88ac
Adamczewski, Szymon <U073251@euc.ppg.com>
2018-11-22 15:42:43 +0100

    ka

--- PubProjectApi/Models/Advertisement.cs
+++ PubProjectApi/Models/Advertisement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -20,5 +21,7 @@ namespace PubProjectApi.Models
         public Boolean Active { get; set; }
         public string Tag { get; set; }
         public virtual GastronomicVenue GastronomicVenue { get; set; }
+
+        //public IFormFile File { get; set; }
     }
 }
